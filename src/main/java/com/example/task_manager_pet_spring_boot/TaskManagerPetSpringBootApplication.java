1325712a55d6a364a45131cf3dddd92398b93a22
IAntonPV <anton_poyylov@rambler.ru>
2025-11-25 23:07:33 +0300

    first commit

---
+++ src/main/java/com/example/task_manager_pet_spring_boot/TaskManagerPetSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_manager_pet_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerPetSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerPetSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/task_manager_pet_spring_boot/TaskManagerPetSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_manager_pet_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerPetSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
